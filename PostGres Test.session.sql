a71ae946106c2168c39901418a34960e52f2aeab
DineshShazam <dineshdinu1607@yahoo.com>
2021-01-05 00:03:51 +0530

    psql

---
+++ PostGres Test.session.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO clarifai_type 
+-- (display,value) VALUES
+-- ('Detect Face',1),
+-- ('Detect Celebrity',2),
+-- ('DemoGraphics',3),
+-- ('Find Food',4)
+
+SELECT * FROM clarifai_type
